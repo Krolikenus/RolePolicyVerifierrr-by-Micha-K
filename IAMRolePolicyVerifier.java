549cd76530ae0bd0b6a3da862adc41fcb6ad1628
Krolikenus <165794073+Krolikenus@users.noreply.github.com>
2024-04-02 14:24:48 +0200

    IAMRolePolicyVerifier.java

--- IAMRolePolicyVerifier.java
+++ IAMRolePolicyVerifier.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class RolePolicyVerifier {
+public class IAMRolePolicyVerifier {
 
     public static boolean sprawdzJSON(File plikWejsciowy) {
         try {
